4db10735526bd0440a378920236eb110b4227e15
Filip Stolarczuk <fifiak1001@gmail.com>
2018-06-03 20:08:57 +0200

    testing adding children

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import xml.etree.ElementTree as et
+import os
 from reader import TextReader
+
 
 reader = TextReader()
 
@@ -6,4 +9,15 @@ data = reader.get_programmes("BBC.json")
 
 print(data)
 
+base_path = os.path.dirname(os.path.realpath(__file__))
+xml_file = os.path.join(base_path, "template.xml")
 
+tree = et.parse(xml_file)
+
+root = tree.getroot()
+
+new_channel = et.SubElement(root, "channel", attrib={"id": "BBC"})
+new_prod_ico = et.SubElement(new_channel, "icon", attrib={"src": "ico.png"})
+
+for child in root:
+    print(child.tag, child.attrib)
